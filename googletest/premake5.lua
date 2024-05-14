32748204e56b12872d3bd30ba15cfbafb7ebda91
babaliaris <babaliaris.nikos@gmail.com>
2024-05-14 20:46:31 +0300

    Fixed debug, dist and release names

--- googletest/premake5.lua
+++ googletest/premake5.lua
@@ -1,69 +1,69 @@
 project "googletest"
-	staticruntime "on"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "c++17"
-	targetdir "%{_WORKING_DIR}/builds/%{cfg.shortname}/"
-	objdir "%{_WORKING_DIR}/obj/%{prj.name}_%{cfg.shortname}/"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "c++17"
+    targetdir "%{_WORKING_DIR}/builds/%{cfg.shortname}/"
+    objdir "%{_WORKING_DIR}/obj/%{prj.name}_%{cfg.shortname}/"
 
-	files {
-		"src/**.h",
-		"src/**.cc",
-		"include/**.h"
-	}
+    files {
+        "src/**.h",
+        "src/**.cc",
+        "include/**.h"
+    }
 
-	includedirs {
-		".",
-		"include/",
+    includedirs {
+        ".",
+        "include/",
 
-	}
-
-
-	-- -------------------------------|All Platforms|------------------------------- --
-	filter{}
-	filter "configurations:Debug"
-		defines "DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter{}
-	filter "configurations:PreRelease"
-		defines "NDEBUG"
-		runtime "Release"
-		optimize "on"
-
-	filter{}
-	filter "configurations:Release"
-		defines "NDEBUG"
-		runtime "Release"
-		optimize "on"
-	-- -------------------------------|All Platforms|------------------------------- --
+    }
 
 
-	-- ----------------------------------|Windows|---------------------------------- --
-	filter{}
-	filter "system:Windows"
-		buildoptions {
-			"-GS", "-W4", "-WX", "-wd4251", "-wd4275", "-nologo", "-J",
-			"-D_UNICODE", "-DUNICODE", "-DWIN32", "-D_WIN32",
-			"-EHs-c-", "-D_HAS_EXCEPTIONS=0", "-GR-", "-wd4702", "-utf-8"
-		}
-		defines {
-			"GTEST_OS_WINDOWS"
-		}
-	-- ----------------------------------|Windows|---------------------------------- --
+    -- -------------------------------|All Platforms|------------------------------- --
+    filter{}
+    filter "configurations:debug"
+        defines "DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter{}
+    filter "configurations:dist"
+        defines "NDEBUG"
+        runtime "Release"
+        optimize "on"
+
+    filter{}
+    filter "configurations:release"
+        defines "NDEBUG"
+        runtime "Release"
+        optimize "on"
+    -- -------------------------------|All Platforms|------------------------------- --
 
 
-	-- -----------------------------------|Linux|----------------------------------- --
-	filter{}
-	filter "system:Linux"
-		buildoptions {
-			"-fno-exceptions", "-Wall", "-Wshadow", "-Wno-error=dangling-else",
-			"-Wextra", "-Wno-unused-parameter", "-Wno-missing-field-initializers"
-		}
-		pic "On"
-		defines {
-			"GTEST_OS_LINUX",
-			"GTEST_HAS_RTTI"
-		}
-	-- -----------------------------------|Linux|----------------------------------- --
+    -- ----------------------------------|Windows|---------------------------------- --
+    filter{}
+    filter "system:Windows"
+        staticruntime "on"
+        buildoptions {
+            "-GS", "-W4", "-WX", "-wd4251", "-wd4275", "-nologo", "-J",
+            "-D_UNICODE", "-DUNICODE", "-DWIN32", "-D_WIN32",
+            "-EHs-c-", "-D_HAS_EXCEPTIONS=0", "-GR-", "-wd4702", "-utf-8"
+        }
+        defines {
+            "GTEST_OS_WINDOWS"
+        }
+    -- ----------------------------------|Windows|---------------------------------- --
+
+
+    -- -----------------------------------|Linux|----------------------------------- --
+    filter{}
+    filter "system:Linux"
+        buildoptions {
+            "-fno-exceptions", "-Wall", "-Wshadow", "-Wno-error=dangling-else",
+            "-Wextra", "-Wno-unused-parameter", "-Wno-missing-field-initializers"
+        }
+        pic "On"
+        defines {
+            "GTEST_OS_LINUX",
+            "GTEST_HAS_RTTI"
+        }
+    -- -----------------------------------|Linux|----------------------------------- --
